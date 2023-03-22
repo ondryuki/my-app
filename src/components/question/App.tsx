712e02d88a0f07cc51bc001e6bfebcd65bbd880f
Onodera Yuki <ondryuki@gmail.com>
2023-03-22 15:34:26 +0900

    feat: 色の変更、問題の追加

--- src/components/question/App.tsx
+++ src/components/question/App.tsx
@@ -3,20 +3,125 @@ import '../../css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="question">
+      <header className="question-header">
+        <h1>四択クイズ</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          全般的な知識についてのクイズです（全5問）
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="question-main">
+        <div>
+          <h2>Q1</h2>
+          <p>江戸幕府が開かれたのは西暦何年？</p>
+          <form action="">
+            <div>
+            <input name="edo" id="1573" type="radio" />
+            <label htmlFor="1573">1573年</label>
+            </div>
+            <div>
+            <input name="edo" id="1586" type="radio" />
+            <label htmlFor="1586">1586年</label>
+            </div>
+            <div>
+            <input name="edo" id="1603" type="radio" />
+            <label htmlFor="1603">1603年</label>
+            </div>
+            <div>
+            <input name="edo" id="1611" type="radio" />
+            <label htmlFor="1611">1611年</label>
+            </div>
+          </form>
+        </div>
+        <div className="questions">
+          <h2>Q2</h2>
+          <p>Nintendo DSが日本で発売されたのはいつ？</p>
+          <form action="">
+            <div>
+            <input name="ds" id="2004" type="radio" />
+            <label htmlFor="2004">2004年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2005" type="radio" />
+            <label htmlFor="2005">2005年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2006" type="radio" />
+            <label htmlFor="2006">2006年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2007" type="radio" />
+            <label htmlFor="2007">2007年11月21日</label>
+            </div>
+          </form>
+        </div>
+        <div className="questions">
+          <h2>Q2</h2>
+          <p>Nintendo DSが日本で発売されたのはいつ？</p>
+          <form action="">
+            <div>
+            <input name="ds" id="2004" type="radio" />
+            <label htmlFor="2004">2004年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2005" type="radio" />
+            <label htmlFor="2005">2005年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2006" type="radio" />
+            <label htmlFor="2006">2006年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2007" type="radio" />
+            <label htmlFor="2007">2007年11月21日</label>
+            </div>
+          </form>
+        </div>
+        <div className="questions">
+          <h2>Q2</h2>
+          <p>Nintendo DSが日本で発売されたのはいつ？</p>
+          <form action="">
+            <div>
+            <input name="ds" id="2004" type="radio" />
+            <label htmlFor="2004">2004年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2005" type="radio" />
+            <label htmlFor="2005">2005年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2006" type="radio" />
+            <label htmlFor="2006">2006年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2007" type="radio" />
+            <label htmlFor="2007">2007年11月21日</label>
+            </div>
+          </form>
+        </div>
+        <div className="questions">
+          <h2>Q2</h2>
+          <p>Nintendo DSが日本で発売されたのはいつ？</p>
+          <form action="">
+            <div>
+            <input name="ds" id="2004" type="radio" />
+            <label htmlFor="2004">2004年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2005" type="radio" />
+            <label htmlFor="2005">2005年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2006" type="radio" />
+            <label htmlFor="2006">2006年11月21日</label>
+            </div>
+            <div>
+            <input name="ds" id="2007" type="radio" />
+            <label htmlFor="2007">2007年11月21日</label>
+            </div>
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
